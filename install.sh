45e63838f6a2c319bdfd557cb285395c4495e1b2
PangPangPangPangPang <446964321@qq.com>
2021-08-21 15:14:13 +0800

    update

--- install.sh
+++ install.sh
@@ -32,3 +32,25 @@ if [ ${machine} == Mac ]; then
     brew install fontconfig
     sh ~/.dotfiles/font/install_font.sh
 fi
+
+if [ ${machine} == Linux ]; then
+    yay -S exa ripgrep alacritty neovim tmux git-delta bat python2-pip python-pip
+    python3 -m pip install --user --upgrade pynvim
+    python -m pip install --user --upgrade pynvim
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+    git clone https://github.com/PangPangPangPangPang/dotfiles.git ~/.dotfiles
+    mkdir -p ~/.config/kitty
+    cp ~/.dotfiles/terminal/kitty.conf ~/.config/kitty
+    cp ~/.dotfiles/terminal/gruvbox-kitty.conf ~/.config/kitty
+    cp ~/.dotfiles/terminal/.zshrc ~/
+    cp ~/.dotfiles/terminal/.zshenv ~/
+    cp ~/.dotfiles/terminal/.p10k.zsh ~/
+
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    cp ~/.dotfiles/terminal/.tmux.conf ~/
+    cp ~/.dotfiles/terminal/tmux.status.conf ~/.tmux.status.conf
+    tmux source ~/.tmux.conf
+    ~/.tmux/plugins/tpm/bin/install_plugins
+    sh ~/.dotfiles/font/install_font.sh
+fi
+
